04c506ba4f61cf2aa63435adc65b35cfdd2799d0
unknown <D0125@SIID.COM.CN>
2019-11-15 18:43:07 +0800

    change

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/yanxitang/vue-press.git master:gh-pages
+git push -f git@github.com:yanxitang/vue-press.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,8 @@ module.exports = {
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base: '/', // 这是部署到github相关的配置
+    // base: '/', // 这是部署到github相关的配置
+    base: '/vue-press/',
     // dest:'.vuepress/public/dist',  //编译输出的目录 
     markdown: {
       lineNumbers: false // 代码块显示行号
