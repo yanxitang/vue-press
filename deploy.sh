4a504fd784978eeb31b880ac70203a9c7d786a6a
unknown <D0125@SIID.COM.CN>
2019-11-15 19:05:59 +0800

    change2

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yanxitang/vue-press.git master:gh-pages
+git push -f git@github.com:yanxitang/vue-press.git master
 
 cd -
